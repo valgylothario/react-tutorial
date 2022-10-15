8283455daf818392c0499cdfec239b303e9660fe
Valgy Sultanigy <valgylothario@gmail.com>
2022-10-15 17:14:13 +0100

    [R-2] - Add dynamic list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 
+const meals = ["Backed Beans", "Backed Sweet Potatoes", "Green Sald", "Pizza"];
+
 function App() {
   return (
     <React.Fragment>
       <h1>Meal List</h1>
-      <h2>Backed Beans</h2>
-      <h2>Backed Sweet Potatoes</h2>
-      <h2>Green Sald</h2>
-      <h2>Pizza</h2>
+      {meals.map((meal, index) => (
+        <h2 key={index}>{meal}</h2>
+      ))}
     </React.Fragment>
   );
 }
